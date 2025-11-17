115cee44bf52532a730e29550c9c3c641e4596f9
suliSarwari20 <sullyyusuf1990@gmail.com>
2025-11-17 10:52:38 +0000

    Create kye17Nov.py

---
+++ kye17Nov.py
@@ -0,0 +1,19 @@
+energylevels = ["4, 5, 3, 1, 2"]
+
+usernames = ["suli","github","python","vscode","nearpod"]
+
+stepcount = ["120","160","180","150","130"]
+
+print(energylevels)
+
+print(usernames)
+
+print(stepcount)
+
+energylen = len(energylevels)
+
+usernamelen = len(username)
+
+print (energylevels[0])
+
+print (energylevels)
